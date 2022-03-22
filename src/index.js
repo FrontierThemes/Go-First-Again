553df471c9699439d1459f1cf9c08ef817c529b1
Arturo Guillén <aguillen.js@gmail.com>
2022-03-21 22:04:47 -0400

    Implemented StimulusJS.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./css/index.css";
+import { Application } from "@hotwired/stimulus";
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
+
+window.Stimulus = Application.start();
+const context = require.context('./controllers', true, /\.js$/);
+Stimulus.load(definitionsFromContext(context));
